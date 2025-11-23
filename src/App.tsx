28e586c374a6dc2e95fc42d1757845f4bb082b57
Alexander1-svg <al.bellod@duocuc.cl>
2025-11-22 23:09:15 -0300

    fix: arreglando errores

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,36 @@
 import { CartProvider } from "./components/CartProvider";
 import { Routes, Route } from "react-router-dom";
 import { Header } from "./components/header";
+import { Footer } from "./components/footer";
 import { HomePage } from "./pages/HomePage";
 import BlogPage from "./pages/BlogPage";
 import CreatePostPage from "./pages/CreatePostPage";
 import CarritoPage from "./pages/CarritoPage";
 import { LoginPage } from "./pages/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage";
-import { DashboardPage } from "./pages/DashboardPage";
 import { NosotrosPage } from "./pages/NosotrosPage";
-import { AuthProvider } from "./pages/AuthContext";
 
 export function App() {
   return (
     <CartProvider>
-<<<<<<< HEAD
-      <AuthProvider>
-        <div className="bg-black font-sans min-h-screen text-white flex flex-col">
-          <Header />
-=======
       <div className="bg-black min-h-screen flex flex-col">
         <Header />
->>>>>>> test
 
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
 
-              <Route path="/nosotros" element={<NosotrosPage />} />
-              <Route path="/blog" element={<BlogPage />} />
-              <Route path="/create-post" element={<CreatePostPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/dashboard" element={<DashboardPage />} />
-              <Route path="/carrito" element={<CarritoPage />} />
-            </Routes>
-          </main>
-        </div>
-      </AuthProvider>
+            <Route path="/nosotros" element={<NosotrosPage />} />
+            <Route path="/blog" element={<BlogPage />} />
+            <Route path="/create-post" element={<CreatePostPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/carrito" element={<CarritoPage />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
     </CartProvider>
   );
 }
